f98a4c33ef643cd57321b7f76458bfd40917ccec
ViktorPresniatski <presniatski@gmail.com>
2017-11-09 19:30:51 +0300

    fix tabs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-	resources :categories, only: [:index]
-	resources :questionnares, only: [:show]
+  resources :categories, only: [:index]
+  resources :questionnares, only: [:show]
   resources :problem_requests, only: [:index]
 end
