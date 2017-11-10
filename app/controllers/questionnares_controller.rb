ab5ab7c545aa993e7e1d6f3fb18c401e2a9743ce
ViktorPresniatski <presniatski@gmail.com>
2017-11-10 20:12:44 +0300

    add action create for questionnares

--- app/controllers/questionnares_controller.rb
+++ app/controllers/questionnares_controller.rb
@@ -1,5 +1,25 @@
 class QuestionnaresController < ApplicationController
+  def new
+    @questionnary = Questionnare.new
+  end
+
+  def create
+    #TODO parsing from params
+    @questionnary = Questionnare.new(questionnary_params)
+    if @questionnary.save
+      redirect_to problem_requests_path
+    else
+      render :new
+    end
+  end
+
   def show
     @questionnary = Questionnare.find(params[:id])
+  end
+
+  private
+
+  def questionnary_params
+    params.require(:questionnary)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :categories, only: [:index]
   resources :problem_requests, only: [:index, :show]
-  resources :questionnares, only: [:show]
+  resources :questionnares, only: [:new, :crerate, :show]
 end
