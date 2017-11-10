ca503ef9688eeda047adbb02e0568a48e10d3e9a
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-11-10 21:09:15 +0300

    Add is_expert, is expert validation

--- app/models/problem_request.rb
+++ app/models/problem_request.rb
@@ -6,11 +6,15 @@ class ProblemRequest < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   def add_response(response_params, current_user)
-    expert_response = ExpertResponse.create(response_params)
-    expert_response.expert = current_user
-    expert_response.problem_request_id = id
-    expert_response.save!
-    expert_responses << expert_response
-    expert_response
+    if (current_user.is_expert)
+      expert_response = ExpertResponse.create(response_params)
+      expert_response.expert = current_user
+      expert_response.problem_request_id = id
+      expert_response.save!
+      expert_responses << expert_response
+      expert_response
+    else
+      errors.add(:base, message: "You not expert")
+    end
   end
 end
