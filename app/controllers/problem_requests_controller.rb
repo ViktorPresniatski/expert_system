30cb80430a90e0a9e0f8b4819ae46afc221a0adb
ViktorPresniatski <presniatski@gmail.com>
2017-11-11 18:30:19 +0300

    add select by category

--- app/controllers/problem_requests_controller.rb
+++ app/controllers/problem_requests_controller.rb
@@ -1,6 +1,12 @@
 class ProblemRequestsController < ApplicationController
   def index
-    @problem_requests = ProblemRequest.all
+    if params[:category_id].present?
+      ids = Category.find_by(id: params[:category_id]).children.ids
+      ids << params[:category_id].to_i
+      @problem_requests = ProblemRequest.joins(:category).where('categories.id IN (?)', ids)
+    else
+      @problem_requests = ProblemRequest.all
+    end
   end
 
   def show
