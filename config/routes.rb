a37f30b50e7045bd78ec947ef1436f1acc4e936a
ViktorPresniatski <presniatski@gmail.com>
2017-11-11 18:55:43 +0300

    fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :categories, only: [:index]
-<<<<<<< HEAD
-  resources :problem_requests, only: [:index, :show] do
-    post 'add_response', to: 'problem_requests#add_response'
-  end
-=======
   resources :problem_requests, only: [:index, :show]
   resources :questionnares, only: [:new, :create, :show]
->>>>>>> 1fd2651c299da77581167eb68b5c358d47134085
 end
